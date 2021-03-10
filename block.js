0a1202bfa4763b38be86dc4837c9b261642fe1c6
shirelysuresh <77004136+shirelysuresh@users.noreply.github.com>
2021-03-10 19:30:19 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,16 @@
+class Block extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+  }
+  display(){
+    console.log(this.body.speed)
+    if(this.body.speed<3){
+    super.display()
+    }
+    else{
+      World.remove(world,this.body)
+    }
+
+  }
+  
+};
